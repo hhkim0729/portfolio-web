9440f6fe2eaaeb65705bf6851c3dc558427ca377
hhkim0729 <42.4.heehkim@gmail.com>
2023-01-25 17:52:27 +0900

    Add arrow up button

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 'use strict';
 
+// transparent navbar
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  const opacity = 1 - window.scrollY / homeHeight;
-  home.style.opacity = opacity;
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// transparent home
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  const homeOpacity = 1 - window.scrollY / homeHeight;
+  home.style.opacity = homeOpacity;
 });
 
 // Handle scrolling when tapping on the navbar menu
@@ -29,6 +34,17 @@ navbarMenu.addEventListener('click', (e) => {
 const contactButton = document.querySelector('.home__contact');
 contactButton.addEventListener('click', () => {
   scrollIntoView('#contact');
+});
+
+// Handle go to top button
+const arrowUp = document.querySelector('.arrow-up');
+arrowUp.addEventListener('click', (e) => scrollIntoView('#home'));
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
 });
 
 function scrollIntoView(selector) {
