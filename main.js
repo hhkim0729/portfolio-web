6bb108fac75c2dfb549a9ef28e606ed04ca8b0c3
hhkim0729 <42.4.heehkim@gmail.com>
2023-01-25 16:53:42 +0900

    Add transparent navbar

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  document.querySelector(link).scrollIntoView({ behavior: 'smooth' });
+});
+
+//
